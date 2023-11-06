9b40ce534ccc7dad0034498af4f225b319fcb2f0
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-11-06 15:19:07 +0530

    Socket IO Chat App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import styles from "./page.module.css";
+import { io } from "socket.io-client";
+import { useState } from "react";
+import ChatPage from "@/components/page";
 
 export default function Home() {
+  const [showChat, setShowChat] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [roomId, setroomId] = useState("");
+
+  var socket: any;
+  socket = io("http://localhost:3001");
+
+  const handleJoin = () => {
+    if (userName !== "" && roomId !== "") {
+      console.log(userName, "userName", roomId, "roomId");
+      socket.emit("join_room", roomId);
+      setShowSpinner(true);
+// You can remove this setTimeout and add your own logic
+      setTimeout(() => {
+        setShowChat(true);
+        setShowSpinner(false);
+      }, 4000);
+    } else {
+      alert("Please fill in Username and Room Id");
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <div
+        className={styles.main_div}
+        style={{ display: showChat ? "none" : "" }}
+      >
+        <input
+          className={styles.main_input}
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUserName(e.target.value)}
+          disabled={showSpinner}
         />
+        <input
+          className={styles.main_input}
+          type="text"
+          placeholder="room id"
+          onChange={(e) => setroomId(e.target.value)}
+          disabled={showSpinner}
+        />
+        <button className={styles.main_button} onClick={() => handleJoin()}>
+          {!showSpinner ? (
+            "Join"
+          ) : (
+            <div className={styles.loading_spinner}></div>
+          )}
+        </button>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div style={{ display: !showChat ? "none" : "" }}>
+        <ChatPage socket={socket} roomId={roomId} username={userName} />
       </div>
-    </main>
-  )
+    </div>
+  );
 }
